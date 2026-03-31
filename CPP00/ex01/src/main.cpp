07bd88c46bee99696de342bb47782f7ebdc04116
namatias <namatias@student.42sp.org.br>
2026-03-31 19:46:34 -0300

    Cpp 00, ex 01

---
+++ CPP00/ex01/src/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/31 19:31:21 by namatias          #+#    #+#             */
+/*   Updated: 2026/03/31 19:45:10 by namatias         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+#include "Contact.hpp"
+#include "iostream"
+#include <cstdio> //para usar printf. retirar //TODO
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1 && !argv[1])
+	{
+		printf("teste\n");
+	}
+	return (0);
+}
