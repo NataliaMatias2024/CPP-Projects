041ecf0fa8c7d4e9179e0fd2e56d2a41b47a634f
namatias <namatias@student.42sp.org.br>
2026-06-19 00:39:15 -0300

    feat: implement Contact class structure and CLI data collection

--- CPP00/ex01/include/Contact.hpp
+++ CPP00/ex01/include/Contact.hpp
@@ -6,31 +6,42 @@
 /*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 19:31:39 by namatias          #+#    #+#             */
-/*   Updated: 2026/04/04 16:06:41 by namatias         ###   ########.fr       */
+/*   Updated: 2026/06/19 00:36:28 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
-#include "iostream"
+#include <iostream>
+//Embora essa biblioteca exista dentro da iostream, ao usar as flags std=c++98
+//PODE SER que o iostream n puxe as outras bibliotecas, por isso é necessario add as outras
+//Ficando para o iostream apenas o controle de fluxo cout e cin
+#include <string>
 
 class Contact
 {
-private:
-	std::string _firstName;
-	std::string _lastName;
-	std::string _nickName;
-	std::string _contactNumber;
-	std::string _darkestSecret;
+	//Tudo que apenas a propria classe deve ter acesso é privado
+	private:
+		size_t		_index;
+		std::string	_firstName;
+		std::string	_lastName;
+		std::string	_nickName;
+		std::string	_phoneNumber;
+		std::string	_secret;
 
-public:
-	Contact(); //Construtor
-	~Contact(); //Destrutot
+	//Métodos da classe e sua inicializaçao e finalizaçao
+	public:
+		Contact(); //Construtor
+		~Contact(); //Destrutot
 
-	void	initContact(void); //inicia o contato
-	void	setContactInfo(void); // seta os valores digitados pelo usuario
-	void	displayContact(int index); //mostra na tela um contato especifico
+		std::string getFirstName() const;
+		std::string getLastName() const;
+		std::string getNickName() const;
+		std::string getPhoneNumber() const;
+		std::string getSecret() const;
+
+		void	setContact();
 };
 
 #endif
--- CPP00/ex01/src/Contact.cpp
+++ CPP00/ex01/src/Contact.cpp
@@ -6,9 +6,63 @@
 /*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 19:31:27 by namatias          #+#    #+#             */
-/*   Updated: 2026/04/01 17:05:26 by namatias         ###   ########.fr       */
+/*   Updated: 2026/06/19 00:37:33 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 
+Contact::Contact()
+{
+	_index = 0;
+}
+
+Contact::~Contact()
+{
+}
+
+std::string Contact::getFirstName() const
+{
+	return (_firstName);
+}
+
+std::string Contact::getLastName() const
+{
+	return (_lastName);
+}
+
+std::string Contact::getNickName() const
+{
+	return (_nickName);
+}
+
+std::string Contact::getPhoneNumber() const
+{
+	return (_phoneNumber);
+}
+
+std::string Contact::getSecret() const
+{
+	return (_secret);
+}
+
+void	Contact::setContact()
+{
+	std::cout << "Plese insert your contact informations:" << std::endl;
+
+	std::cout << "First Name: ";
+	std::getline(std::cin, _firstName);
+
+	std::cout << "Last Name: ";
+	std::getline(std::cin, _lastName);
+
+	std::cout << "Nickname: ";
+	std::getline(std::cin, _nickName);
+
+	std::cout << "Phone Number: ";
+	std::getline(std::cin, _phoneNumber);
+
+	std::cout << "Darkest Secret: ";
+	std::getline(std::cin, _secret);
+
+}
