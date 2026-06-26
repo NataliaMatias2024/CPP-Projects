dc37cf59fe8e3bb571f5e65273d525882561683b
namatias <namatias@student.42sp.org.br>
2026-06-26 01:31:54 -0300

    refactor: att functions

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 13:49:35 by namatias          #+#    #+#             */
-/*   Updated: 2026/03/31 18:10:44 by namatias         ###   ########.fr       */
+/*   Updated: 2026/06/26 01:30:01 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 int main(int argc, char *argv[])
 {
 	int	i;
+	std::string str;
 
     if (argc == 1) // se n escrever argumento nenhum imprime a msg base
     {
@@ -28,11 +29,10 @@ int main(int argc, char *argv[])
 		i = 1; //argc 0 é o nome do programa, por isso n consideramos
 		while (i < argc) // irá andar pelos argumentos, cada i é um argumento inteiro
 		{
-			std::string str = argv[i];//while preenche a primeira palavra/argumento
+			str = argv[i];	//while preenche a primeira palavra/argumento
 			for (size_t j = 0; j < str.length(); j++) // anda letra por letra, ate deixar tds em caps
-			{
-				std::cout << (char)std::toupper(str[j]);//for transforma a primeira letra em maiusculo
-			}
+				std::cout << (char)std::toupper(str[j]);	//for transforma a primeira letra em maiusculo
+
 			if (i < argc - 1)
 				std::cout << " ";
 			i++;
