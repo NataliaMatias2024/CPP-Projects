d0ff5831ff9be06abd2bc6b0f8b4acc97108387e
namatias <namatias@student.42sp.org.br>
2026-07-01 11:16:39 -0300

    feat(ex00): implement Zombie class and basic behaviors
    - Create Zombie class with member initializer list
    - Add announce() and debug destructor
    - Declare prototypes for memory allocation tests (newZombie and randomChump)

--- CPP01/include/Zombie.hpp
+++ CPP01/include/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: namatias <namatias@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/30 12:45:32 by namatias          #+#    #+#             */
-/*   Updated: 2026/06/30 12:58:35 by namatias         ###   ########.fr       */
+/*   Updated: 2026/07/01 10:51:22 by namatias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,20 @@ class Zombie
 		std::string _name;
 
 	public:
-		Zombie();
+		Zombie(std::string name);
+	//n conseguimos passar parametros para o destrutor, apenas para o construtor
 		~Zombie();
 
-		void	announce(void);
+		void	announce(void) const;
 };
 
+//new é o equivalente ao malloc, em C++ essa key reserva espaço d memoria na Heap
+//por isso esse zumbi, objeto, existirá até alguem chamar um delete (free do C)
 Zombie*	newZombie(std::string name);
+
+//Esse zumbi nao será usado depois, podendo morrer logo depois que for chamado
+//Nao terá alocação de memoria, encerrando após a função q o chamou encerrar
+//Por isso fica na memoria Stack (alocação local)
 void	randomChump(std::string name);
 
 #endif
